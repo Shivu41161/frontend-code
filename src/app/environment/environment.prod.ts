3e2cec24461a12efff7ee1f51ac6492495f591b9
S D, Shivaraj <shivaraj.sd@championx.com>
2025-02-28 12:55:23 +0530

    render url updated

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://your-backend.onrender.com/api/items' // Render backend URL
+    apiUrl: 'https://backend-code-oxv1.onrender.com/api/items' // Render backend URL
   };
   
